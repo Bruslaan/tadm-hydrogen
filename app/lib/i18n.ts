1c432fa62dbdb6e3f879ad1adeb455b42f0fa3fc
Ruslan Noschajew <bruslan.no@gmail.com>
2025-07-04 17:11:23 +0200

    Revert "Add country and locale configuration for improved internationalization"
    This reverts commit fba1029bc4ef19a22ed119427f7c89b04d6535ca.

--- app/lib/i18n.ts
+++ app/lib/i18n.ts
@@ -4,25 +4,18 @@ export interface I18nLocale extends I18nBase {
   pathPrefix: string;
 }
 
-export const SUPPORTED_LOCALES = ['en-US', 'de-DE'] as const;
-export type SupportedLocale = typeof SUPPORTED_LOCALES[number];
-
 export function getLocaleFromRequest(request: Request): I18nLocale {
   const url = new URL(request.url);
-  const firstPathPart = url.pathname.split('/')[1]?.toLowerCase() ?? '';
+  const firstPathPart = url.pathname.split('/')[1]?.toUpperCase() ?? '';
 
   type I18nFromUrl = [I18nLocale['language'], I18nLocale['country']];
 
   let pathPrefix = '';
   let [language, country]: I18nFromUrl = ['EN', 'US'];
 
-  if (/^[a-z]{2}-[a-z]{2}$/i.test(firstPathPart)) {
-    const normalizedLocale = firstPathPart.toLowerCase();
-    
-    if (SUPPORTED_LOCALES.includes(normalizedLocale as SupportedLocale)) {
-      pathPrefix = '/' + normalizedLocale;
-      [language, country] = normalizedLocale.split('-').map(part => part.toUpperCase()) as I18nFromUrl;
-    }
+  if (/^[A-Z]{2}-[A-Z]{2}$/i.test(firstPathPart)) {
+    pathPrefix = '/' + firstPathPart;
+    [language, country] = firstPathPart.split('-') as I18nFromUrl;
   }
 
   return {language, country, pathPrefix};
